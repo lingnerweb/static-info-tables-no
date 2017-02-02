ffe4f726be0ad941ca0cbf2d1dcfe35360582099
Markus Klein <markus.klein@reelworx.at>
2017-02-02 11:44:53 +0100

    [INIT] Import of old version 0.0.2

---
+++ ext_emconf.php
@@ -0,0 +1,26 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Static Info Tables (no)',
+	'description' => 'Norwegian language pack for Static Info Tables.',
+	'category' => 'misc',
+	'version' => '0.0.2',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '',
+	'PHP_version' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Mathas Bolt Lesniak, LiliO Design',
+	'author_email' => 'mathias@lilio.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
